bddd2066aeb0420f01788129f9147ffa0fb94700
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-11-15 17:11:28 +0100

    Implemented some layout

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Main.css";
+import Printform from "./printform/Printform";
 
 export default function Main() {
   return (
     <div>
-      <div style={{ height: "350px", backgroundColor: "greenyellow" }}>
+      <div style={{ height: "465px", backgroundColor: "greenyellow" }}>
         Image
       </div>
       <main style={{ display: "flex", justifyContent: "center" }}>
@@ -17,15 +18,17 @@ export default function Main() {
           }}
         >
           <div style={{ width: "50%", height: "500px", background: "white" }}>
-            <div style={{ width: "80%", margin: "30px" }}>
-              <h1>UAERO PRINTING -A modern printing solutions</h1>
+            <div style={{width: "70%", marginTop: "30px", marginLeft: "30px" }}>
+              <h1>UAERO PRINTING -Modern 3D printing solutions</h1>
               <p style={{ color: "#29ABE2", fontWeight: "bold" }}>
-                Power by Ursvik Aerodunamics
+                Power by Ursvik Aerodynamics
               </p>
             </div>
           </div>
-          <div style={{ width: "50%", height: "500px", background: "orange" }}>
-            Div 2
+          <div style={{ width: "50%", height: "500px", display: "flex", justifyContent: "right" }}>
+            <div style={{ width:"70%",background: "orange", marginTop: "30px", marginRight: "30px" }}>
+              <Printform />
+            </div>
           </div>
         </div>
       </main>
