e0d571e7c0e107b0a2579c1be4c9d8c73b8fa7bb
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-11-15 20:49:50 +0100

    image

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,6 +5,8 @@ import Upload from "./images/icons8-upload-100.png";
 import Quote from "./images/icons8-receipt-approved-100.png";
 import Shipped from "./images/icons8-shipped-100.png";
 
+
+
 export default function Main() {
   return (
     <div>
@@ -21,20 +23,34 @@ export default function Main() {
           }}
         >
           <div style={{ width: "50%", height: "500px", background: "white" }}>
-            <div style={{width: "70%", marginTop: "30px", marginLeft: "30px" }}>
+            <div style={{ width: "70%", marginTop: "30px", marginLeft: "30px" }}>
               <h1>UAERO PRINTING -Modern 3D printing solutions</h1>
               <p style={{ color: "#29ABE2", fontWeight: "bold" }}>
                 Power by Ursvik Aerodynamics
               </p>
             </div>
-            <div>
-              <img src={Upload} alt="Upload" />
-              <img src={Quote} alt="Quote" />
-              <img src={Shipped} alt="Shipped" />
+            <div style={{ display: "flex" , flexDirection: "column", marginLeft: "30px" }}>
+              <div className="icon" style={{ display: "flex", flexDirection: "row" }}>
+                <h1>1.</h1>
+                <img className="marginLeftRight" src={Upload} alt="Upload" />
+                <p>Upload you cad</p>
+              </div>
+              <div className="icon" style={{ display: "flex", flexDirection: "row" }}>
+                <h1>2.</h1>
+                <img className="marginLeftRight" src={Quote} alt="Quote" />
+                <p>Recive a price quote and pay</p>
+              </div>
+              <div className="icon" style={{ display: "flex", flexDirection: "row" }}>
+                <h1>3.</h1>
+                <img className="marginLeftRight" src={Shipped} alt="Shipped" />
+                <p>We print and ship</p>
+              </div>
+            </div>
+            <div className="printArt1">
             </div>
           </div>
           <div style={{ width: "50%", height: "500px", display: "flex", justifyContent: "right" }}>
-            <div style={{ width:"70%",background: "orange", marginTop: "30px", marginRight: "30px" }}>
+            <div style={{ borderRadius: "20px", width: "70%", background: "#808080", marginTop: "30px", marginRight: "30px" }}>
               <Printform />
             </div>
           </div>
