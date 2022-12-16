ab729babb5aadcad47175f0145a325375e4eedd4
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-12-16 21:05:00 +0100

    -

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes ,Route } from "react-router-dom";
+import Login from "./components/main/accounts/Login";
+import Register from "./components/main/accounts/Register";
 
 
 const root = ReactDOM.createRoot(
@@ -14,6 +16,9 @@ root.render(
     <React.StrictMode>
       <Routes>
         <Route path="/" element={<App/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Login />} />
+
       </Routes>
     </React.StrictMode>
   </Router>
